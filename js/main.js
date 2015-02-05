951acdb17366cbf00273d96e56fe6f3acb526fbe
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-05 16:45:35 -0500

    complete the numba function

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function stringLengthChecker( string ) {
+    if (string.length < 7 ){
+        console.log( "What a short little word!" );
+    } else if (string.length > 7 ){
+        console.log( "I'm sorry, but that’s too many to count." );
+    } else {
+        console.log( "7, what a perfect choice!" );
+    }
+}
+
+numba( "7" );
+numba( "seventy" );
+numba( "university" );
