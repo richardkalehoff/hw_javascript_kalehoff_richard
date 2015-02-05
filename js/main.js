2754176d6923ddfc391e7c9b9f19552c9633be68
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-05 16:42:18 -0500

    add presidential information

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var presidents = [
+    {
+        firstName: "Barack",
+        lastName: "Obama",
+        termLength: 2,
+        party: "D",
+        yearsOfPresidency: 8
+    },
+    {
+        firstName: "George W.",
+        lastName: "Bush",
+        termLength: 2,
+        party: "R",
+        yearsOfPresidency: 8
+    },
+    {
+        firstName: "Bill",
+        lastName: "Clinton",
+        termLength: 2,
+        party: "D",
+        yearsOfPresidency: 8
+    },
+    {
+        firstName: "George",
+        lastName: "Bush",
+        termLength: 1,
+        party: "R",
+        yearsOfPresidency: 4
+    },
+    {
+        firstName: "Ronald",
+        lastName: "Reaan",
+        termLength: 2,
+        party: "R",
+        yearsOfPresidency: 8
+    }
+];
+
+console.log( presidents[2].firstName + " " + presidents[2].lastName );
